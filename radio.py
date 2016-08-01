a8e6ca1d18a3654642d378f8c0dd47cd3c830e09
ericka1599 <erickasegura96@gmail.com>
2016-08-01 08:28:06 -0600

    :)

--- radio.py
+++ radio.py
@@ -1,7 +1,10 @@
 class Radio ():
-    def __init__ (self, estacion):
+    def __init__ (self, estacion_FM, FM, estacion_AM ):
         self.encendido = True
-        self.estacion = 88.00
+        self.estacion_FM = 88.00
+        self.estacion_AM = 300
+        self.FM = True
+
 
     def encender (self):
         self.encender = True
@@ -9,17 +12,34 @@ class Radio ():
     def apagar (self):
         self.apagar = False
 
-    def estacion (self):
-        while input ("Desea subir o bajar de estacion? si/no ") == "si":
-            if input("¿Arriba o abajo? ") == "arriba":
-                estacion = estacion + 0.5
-            else :
-                estacion = estacion - 0.5
+    def estacion_FM (self):
+       # des = int(input("1. Subir de estacion \n 2. Bajar de estacion \n 3. Dejar igual la estacion"))
 
-            if estacion > 107.50 :
-                estacion = 88.00
+        if des == 1:
+            estacion_FM = estacion_FM + 0.5
+        else :
+            estacion_FM = estacion_FM - 0.5
 
-            if estacion < 88.00 :
-                estacion = 107.50
+        if estacion_FM > 107.00 :
+            estacion_FM = 87.00
 
-        self.estacion = estacion
+        if estacion_FM < 88.00 :
+            estacion_FM = 107.00
+
+        self.estacion_FM = estacion_FM
+
+    def estacion_FM (self):
+       # des = int(input("1. Subir de estacion \n 2. Bajar de estacion \n 3. Dejar igual la estacion"))
+
+        if des == 1:
+            estacion_AM = estacion_AM + 40
+        else :
+            estacion_AM = estacion_AM - 40
+
+        if estacion_AM > 1300 :
+            estacion_AM = 300
+
+        if estacion_AM < 1300 :
+            estacion_AM = 300
+
+        self.estacion_AM = estacion_AM
